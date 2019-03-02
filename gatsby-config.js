041c3e9bab2b2e7a9a3393d831ac057b7faf8d23
Steven Bruno <sbruno636@gmail.com>
2019-03-02 17:56:24 -0600

    set page title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Steven Bruno`,
+    description: `The personal website of Steven Bruno`,
+    author: `Steven Bruno`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
