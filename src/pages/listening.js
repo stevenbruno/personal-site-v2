44179cc9d25379a7f17223db2ee4e672af5d3646
Steven Bruno <sbruno636@gmail.com>
2019-03-02 18:12:46 -0600

    create primary pages

---
+++ src/pages/listening.js
@@ -0,0 +1,13 @@
+import React from "react"
+import LeftNav from "../components/leftNav"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ListeningPage = () => (
+  <Layout>
+    <SEO title="Listening" />
+    <LeftNav />
+  </Layout>
+)
+
+export default ListeningPage
---
+++ src/pages/writing.js
@@ -0,0 +1,13 @@
+import React from "react"
+import LeftNav from "../components/leftNav"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const WritingPage = () => (
+  <Layout>
+    <SEO title="Writing" />
+    <LeftNav />
+  </Layout>
+)
+
+export default WritingPage
