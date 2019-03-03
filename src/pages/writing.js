d8c44b63893170287ec7171aa366b6523840b5d1
Steven Bruno <sbruno636@gmail.com>
2019-03-03 17:04:16 -0600

    fix file paths

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,6 +1,6 @@
 import React from "react"
-import LeftNav from "../components/leftNav"
-import Layout from "../components/layout"
+import LeftNav from "../components/Leftnav/leftNav"
+import Layout from "../components/Layout/layout"
 import SEO from "../components/seo"
 
 const WritingPage = () => (
