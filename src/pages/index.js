82f08f5af4a4a4cd342941d32dfaf2599c05579f
Steven Bruno <sbruno636@gmail.com>
2019-03-02 18:58:54 -0600

    add homepage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,20 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <LeftNav />
+    <div>
+      <h1>Steven Bruno</h1>
+      <p>
+        I'm a web designer and developer working in Chicago. 
+        Before this, I studied Civil Engineering (Northwestern University '18)
+      </p>
+      <p>
+        At work, I focus on front-end web technologies, but at home, 
+        I enjoy anything and everything Python. 
+      </p>
+      <p>
+        Reach me at <span>sbruno636@gmail.com</span>
+      </p>
+    </div>
   </Layout>
 )
 
