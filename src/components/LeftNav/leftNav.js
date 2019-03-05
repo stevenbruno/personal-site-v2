ef063ce153396ef7b3817c9917c0b921ca0844a0
Steven Bruno <sbruno636@gmail.com>
2019-03-04 23:36:05 -0600

    style active items in left nav

--- src/components/LeftNav/leftNav.js
+++ src/components/LeftNav/leftNav.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 
 const NavLink = props => (
   <li>
-    <Link to={props.to}>{props.children}</Link>
+    <Link to={props.to} activeClassName={navStyles.active}>{props.children}</Link>
   </li>
 )
 
