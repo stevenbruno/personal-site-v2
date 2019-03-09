e6298f097c199b6bb09112ef65a1d560f720d417
Steven Bruno <sbruno636@gmail.com>
2019-03-09 17:55:36 -0600

    add social links

--- src/components/LeftNav/leftNav.js
+++ src/components/LeftNav/leftNav.js
@@ -1,6 +1,14 @@
 import React from "react"
 import navStyles from "./leftNav.module.css"
 import { Link } from "gatsby"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// create fontawesome library
+library.add(
+  fab
+)
 
 const NavLink = props => (
   <li>
@@ -18,11 +26,23 @@ export default () => (
     </nav>
     <div>
       <h3>Social</h3>
-      <div>Icons</div>
+      <div className={navStyles.social}>
+        <a href="https://www.linkedin.com/in/stevenjbruno/" target="_blank">
+          <FontAwesomeIcon icon={['fab', 'linkedin']} size='2x' />
+        </a>
+        <a href="https://dev.to/stevenbruno" target="_blank">
+          <FontAwesomeIcon icon={['fab', 'dev']} size='2x' />
+        </a>
+        <a href="https://github.com/stevenbruno" target="_blank">
+          <FontAwesomeIcon icon={['fab', 'github-square']} size='2x' />
+        </a>
+      </div>
     </div>
     <div>
       <h3>Contact</h3>
-      <p>sbruno636@gmail.com</p>
+      <p className={navStyles.email}>
+        <a href="mailto:sbruno636@gmail.com">sbruno636@gmail.com</a>
+      </p>
     </div>
   </div>
 )
