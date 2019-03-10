6d9bb8d17bca369433a045e504c0afb5bb76d239
Steven Bruno <sbruno636@gmail.com>
2019-03-10 17:06:02 -0500

    change image to fixed

--- src/components/image.js
+++ src/components/image.js
@@ -19,14 +19,14 @@ const Image = () => (
       query {
         file(relativePath: { eq: "images/headshot-bw.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
+            fixed(width: 300) {
+              ...GatsbyImageSharpFixed
             }
           }
         }
       }
     `}
-    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+    render={data => <Img fixed={data.placeholderImage.childImageSharp.fixed} />}
   />
 )
 export default Image
