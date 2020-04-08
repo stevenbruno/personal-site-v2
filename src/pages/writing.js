f30e5176cc31c46ecedebfd3df8bb197caa84417
Steven Bruno <sbruno636@gmail.com>
2020-04-08 18:30:35 -0500

    get my articles from dev.to api

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,17 +1,29 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import LeftNav from "../components/LeftNav/leftNav"
 import Layout from "../components/Layout/layout"
 import SEO from "../components/seo"
 import Content from "../components/Content/content"
 
-const WritingPage = () => (
-  <Layout>
-    <SEO title="Writing" />
-    <LeftNav />
-    <Content>
-      <h1>Writing</h1>
-    </Content>
-  </Layout>
-)
+const WritingPage = () => {
+  const [articles, setArticles] = useState({})
+  useEffect(() => {
+    // get data from devto api
+    fetch("https://dev.to/api/articles?username=stevenbruno")
+      .then(response => response.json()) // parse JSON from request
+      .then(resultData => {
+        setArticles(resultData)
+      })
+  }, [])
+  console.log({ articles })
+  return (
+    <Layout>
+      <SEO title="Writing" />
+      <LeftNav />
+      <Content>
+        <h1>Writing</h1>
+      </Content>
+    </Layout>
+  )
+}
 
 export default WritingPage
