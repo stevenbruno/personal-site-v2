abce608b5f05721c492ec1b10ad8a55f3966b5a1
Steven Bruno <sbruno636@gmail.com>
2020-04-17 22:04:11 -0500

    remove useless graphql query

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,28 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
 import "./layout.css"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <div className="topSplash"></div>
-        <main>{children}</main>
-        <div className="triangle"></div>
-        <div className="triangle2"></div>
-      </>
-    )}
-  />
+  <>
+    <div className="topSplash"></div>
+    <main>{children}</main>
+    <div className="triangle"></div>
+    <div className="triangle2"></div>
+  </>
 )
 
 Layout.propTypes = {
